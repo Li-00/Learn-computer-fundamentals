f1ac2e4be5b72319fd53d38cdfe09e9fcdd1c94d
zhenyu <zhenyu_li1998@163.comssh-keygen -t rsa -C zhenyu_li1998@163.comcat ~/.ssh/id_rsa.pubgit config --global user.email zhenyu_li1998@163.com>
2022-02-26 13:52:33 +0800

    init

---
+++ src/main/java/com/example/eight_part_essay/EightPartEssayApplication.java
@@ -0,0 +1,13 @@
+package com.example.eight_part_essay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EightPartEssayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EightPartEssayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/eight_part_essay/EightPartEssayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eight_part_essay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EightPartEssayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
